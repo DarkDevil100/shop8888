cb8dbf926c641a5bd9da15282bb5161a84b7ba94
DarkDevil100 <vinothshanmugamss7@gmail.com>
2025-04-16 21:22:04 +0530

    Initial commit: Basic express app setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.get("/" , (req, res) => {
+    res.send("Welcome to Shop8888!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is Up & Running on port ${PORT}`);
+})
